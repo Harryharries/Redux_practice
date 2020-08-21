8af92598638c8d29021282386a25d02d97386873
Harryharries <a1021604754@gmail.com>
2020-08-21 00:14:18 -0500

    depatched UI and bussines

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import "antd/dist/antd.css";
-import {Input, Button, List} from 'antd'
 import store from './store'
 import {changeInputAction, addItemAction, deleteItemAction} from './store/actionCreators'
+import TodoListUI from './TodoListUI'
 
 class TodoList extends Component {
     constructor(props) {
@@ -11,33 +10,17 @@ class TodoList extends Component {
         this.changeInputValue= this.changeInputValue.bind(this)
         this.storeChange = this.storeChange.bind(this)
         this.clickBtn = this.clickBtn.bind(this)
+        this.deleteItem=this.deleteItem.bind(this)
         store.subscribe(this.storeChange)
     }
     render() { 
-        return ( <div style ={{margin:'30px'}}>
-            <div>
-    {/* <h1>{this.state.inputValue}</h1> */}
-            <Input 
-            placeholder={this.state.inputValue}
-            style={{width: '250px', marginRight:'10px'}}
-            onChange={this.changeInputValue}
-            value = {this.state.inputValue}
-            />
-
-            <Button 
-            type="primary"
-            onClick={this.clickBtn}> add </Button>
-            </div>
-            
-            <div style={{margin:'10px',width:'300ox'}}>
-                <List
-                bordered
-                dataSource={this.state.list}
-                renderItem={(item,index)=>(
-                <List.Item onClick={this.deleteItem.bind(this,index)}>{item}</List.Item>
-                )}></List>
-            </div>
-            </div> 
+        return ( 
+            <TodoListUI 
+            inputValue={this.state.inputValue}
+            changeInputValue={this.changeInputValue}
+            clickBtn={this.clickBtn}
+            list={this.state.list}
+            deleteItem={this.deleteItem}/>
 );
     }
 
---
+++ src/TodoListUI.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import "antd/dist/antd.css";
+import {Input, Button, List} from 'antd'
+
+class TodoListUI extends Component {
+    render() { 
+        return ( 
+<div style ={{margin:'30px'}}>
+            <div>
+    {/* <h1>{this.state.inputValue}</h1> */}
+            <Input 
+            placeholder={this.props.inputValue}
+            style={{width: '250px', marginRight:'10px'}}
+            onChange={this.props.changeInputValue}
+            value = {this.props.inputValue}
+            />
+
+            <Button 
+            type="primary"
+            onClick={this.props.clickBtn}> add </Button>
+            </div>
+            
+            <div style={{margin:'10px',width:'300ox'}}>
+                <List
+                bordered
+                dataSource={this.props.list}
+                renderItem={(item,index)=>(
+                <List.Item onClick={()=>(this.props.deleteItem(index))}>{item}</List.Item>
+                )}></List>
+            </div>
+            </div> 
+         );
+    }
+}
+ 
+export default TodoListUI;
